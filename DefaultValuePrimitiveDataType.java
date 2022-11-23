6ea7bd45e9f5e667adefdee541fef68684749e43
ravi406 <ravikamble406@gmail.com>
2022-11-23 18:10:18 +0530

    Added default values primitive datatype program

---
+++ DefaultValuePrimitiveDataType.java
@@ -0,0 +1,22 @@
+
+public class DefaultValuePrimitiveDataType {
+	int a;
+	float b;
+	char c;
+	double d;
+	long l;
+	boolean bool;
+	
+	public static void main(String[] args) {
+		DefaultValuePrimitiveDataType obj = new DefaultValuePrimitiveDataType();
+		System.out.println("Default value of Int : "+obj.a);
+		System.out.println("Default value of float : "+obj.b);
+		System.out.println("Default value of char : "+obj.c);
+		System.out.println("Default value of double : "+obj.d);
+		System.out.println("Default value of long : "+obj.l);
+		System.out.println("Default value of boolean : "+obj.bool);
+		
+		
+	}
+
+}
