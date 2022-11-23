0189a4e319bf4eb7f39a73bfc8d5d146717892c6
ravi406 <ravikamble406@gmail.com>
2022-11-23 18:13:08 +0530

    Added string comparision program

---
+++ StringEqualOrNot.java
@@ -0,0 +1,11 @@
+
+public class StringEqualOrNot {
+
+	public static void main(String[] args) {
+		String s1 = "Ravi";
+		String s2 = "kamble";
+//		String s3 = "ravi";
+
+		System.out.println(s1.toLowerCase().equals(s2));
+	}
+}
